3f7960676da441378392d615075cea36edc76a98
shizantnu <142576200+shizantnu@users.noreply.github.com>
2026-01-26 16:17:26 +0100

    chore: setup final husky configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    languageOptions: { globals: {...globals.browser, ...globals.node} } 
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }
+]);
