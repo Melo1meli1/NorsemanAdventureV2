1462d48dbd17c4081d21261d7cad03ee63bc2c67
shizantnu <142576200+shizantnu@users.noreply.github.com>
2026-01-28 16:49:16 +0100

    feat: created migrations for tours table

---
+++ na/app/migrations/tours/tours.sql
@@ -0,0 +1,26 @@
+create type tour_status as enum ('draft', 'published');
+
+create table public.tours (
+  id uuid primary key default gen_random_uuid(),
+
+  title text not null,
+  description text,
+  price numeric(10, 2) not null,
+
+  start_date timestamptz not null,
+  end_date timestamptz,
+
+  seats_available integer not null,
+  image_url text,
+
+  status tour_status not null default 'draft',
+
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+
+create trigger set_tours_updated_at
+before update on public.tours
+for each row
+execute function public.set_current_timestamp_updated_at();
+
